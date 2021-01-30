fb9dce20f1434fa18ce2ccc70155e8e9bc192662
GitHub Action <action@github.com>
2021-01-30 10:44:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/sedyjaku/generatecrudpluginv2/MyBundle.kt
+++ src/main/kotlin/com/github/sedyjaku/generatecrudpluginv2/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sedyjaku.generatecrudpluginv2
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/sedyjaku/generatecrudpluginv2/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sedyjaku.generatecrudpluginv2.services
+
+import com.github.sedyjaku.generatecrudpluginv2.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
